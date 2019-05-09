cf0b1fdb3ec7e9f9c5e77e7eff698f756a1f6b7c
Andy Stobirski <AndyStobirski@users.noreply.github.com>
2019-05-09 11:35:23 +0100

    Added unit tests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLAPI_Consumer;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Insert KEY here
+        private string key => "";
+
+        [TestMethod]
+        public void Test_UfnTFGM_Get()
+        {
+            System.Data.SqlTypes.SqlString output = Functions.UfnTFGM_Get("https://api.tfgm.com/odata/Metrolinks", key, null, null, null, null, null, null, null);
+
+            File.WriteAllText("foo.txt", output.ToString());
+
+            Console.WriteLine(output);
+        }
+    }
+}
